43ab29cb4dcbbe4673d7bc7fbb3f351a3b1c4a9e
Gian Ryu <144473604+Gian-Ryu@users.noreply.github.com>
2025-04-30 14:47:44 -0400

    Update alternativeAssignment.java

--- src/alternativeAssignment.java
+++ src/alternativeAssignment.java
@@ -1,20 +1,25 @@
 import java.io.FileNotFoundException;
+import java.util.ArrayLists;
 
 public class alternativeAssignment {
     public static void main(String[] args) throws FileNotFoundException {
         int count = 1;
+        ArrayList() done = new ArrayList()<int>;
         for (int s = 1; s <= 5000; s++) {
             for (int period = 1; period <= 10; period++) { //loop for periods
                 // @class_id is a user defined variable; use this to get the class_id of the student in this period
                 System.out.println("SELECT classid_" + period + " INTO @class_id FROM StudentSchedule WHERE student_id = " + s + ";");
-
-                for (int n = 1; n <= 12; n++) {
-                    System.out.println("INSERT INTO Assignment (assignment_id, class_id, weight) VALUES (" + count + ", @class_id, 'minor');");
-                    count++;
-                }
-                for (int n = 13; n <= 15; n++) {
-                    System.out.println("INSERT INTO Assignment (assignment_id, class_id, weight) VALUES (" + count + ", @class_id, 'major');");
-                    count++;
+                if (!done.contains(@class_id))
+                {
+                    done.add(@class_id);
+                    for (int n = 1; n <= 12; n++) {
+                        System.out.println("INSERT INTO Assignment (assignment_id, class_id, weight) VALUES (" + count + ", @class_id, 'minor');");
+                        count++;
+                    }
+                    for (int n = 13; n <= 15; n++) {
+                        System.out.println("INSERT INTO Assignment (assignment_id, class_id, weight) VALUES (" + count + ", @class_id, 'major');");
+                        count++;
+                    }
                 }
             }
         }
