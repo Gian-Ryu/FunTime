320ef87ea2aeb6b3b633ec43ee07a445df9a0ade
maggiez33 <145692117+maggiez33@users.noreply.github.com>
2025-05-01 14:40:51 -0400

    Add files via upload

--- src/alternativeAssignment.java
+++ src/alternativeAssignment.java
@@ -1,29 +1,27 @@
 import java.io.FileNotFoundException;
-import java.util.ArrayLists;
+import java.util.ArrayList;
 
 public class alternativeAssignment {
     public static void main(String[] args) throws FileNotFoundException {
         int count = 1;
-        ArrayList<Integer> done = new ArrayList();
-        for (int s = 1; s <= 5000; s++) {
-            for (int period = 1; period <= 10; period++) { //loop for periods
-                // @class_id is a user defined variable; use this to get the class_id of the student in this period
-                System.out.println("SELECT classid_" + period + " INTO @class_id FROM StudentSchedule WHERE student_id = " + s + ";");
-                if (!done.contains(@class_id))
-                {
-                    done.add(@class_id);
-                    for (int n = 1; n <= 12; n++) {
-                        System.out.println("INSERT INTO Assignment (assignment_id, class_id, weight) VALUES (" + count + ", @class_id, 'minor');");
-                        count++;
-                    }
-                    for (int n = 13; n <= 15; n++) {
-                        System.out.println("INSERT INTO Assignment (assignment_id, class_id, weight) VALUES (" + count + ", @class_id, 'major');");
-                        count++;
-                    }
+        for (int c = 1; c <= 340; c++) { //loop for periods
+            // @class_id is a user defined variable; use this to get the class_id of the student in this period
+                for (int n = 1; n <= 12; n++) {
+                    System.out.println("INSERT INTO Assignment (assignment_id, class_id, weight) VALUES (" + count + ", " + c + ", 'minor');");
+                    count++;
                 }
-            }
+                for (int n = 13; n <= 15; n++) {
+                    System.out.println("INSERT INTO Assignment (assignment_id, class_id, weight) VALUES (" + count + ", " + c + ", 'major');");
+                    count++;
+                }
+
         }
 
     }
 }
 
+/*
+CREATE TABLE Assignment ( assignment_id integer, class_id integer, weight varchar(255),
+    PRIMARY KEY (assignment_id),
+    FOREIGN KEY (class_id) REFERENCES Class(class_id) );
+ */
