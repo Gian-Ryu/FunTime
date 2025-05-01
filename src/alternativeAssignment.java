cf6b7da338042b68cbe84cc49673babff4bc44d5
maggiez33 <145692117+maggiez33@users.noreply.github.com>
2025-05-01 11:48:04 -0400

    Update alternativeAssignment.java

--- src/alternativeAssignment.java
+++ src/alternativeAssignment.java
@@ -4,7 +4,7 @@ import java.util.ArrayLists;
 public class alternativeAssignment {
     public static void main(String[] args) throws FileNotFoundException {
         int count = 1;
-        ArrayList() done = new ArrayList()<int>;
+        ArrayList<Integer> done = new ArrayList();
         for (int s = 1; s <= 5000; s++) {
             for (int period = 1; period <= 10; period++) { //loop for periods
                 // @class_id is a user defined variable; use this to get the class_id of the student in this period
